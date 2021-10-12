87aefe320d54b1f6170bab5dd7c4c84e75a0b104
leman789 <leman3211@wp.pl>
2021-10-12 08:11:41 +0200

    RecyclerView Zrobiona Lista

---
+++ RisajklerView/app/src/main/java/com/example/risajklerview/ListaProduktow.java
@@ -0,0 +1,24 @@
+package com.example.risajklerview;
+
+import java.util.LinkedList;
+
+public class ListaProduktow {
+    public static LinkedList<String> ListaProduktow = new LinkedList<>();
+    public ListaProduktow(){
+        ListaProduktow.addLast("Mleko");
+        ListaProduktow.addLast("Jogurt");
+        ListaProduktow.addLast("Jabuszka");
+        ListaProduktow.addLast("Gruszki");
+        ListaProduktow.addLast("Parowki");
+        ListaProduktow.addLast("Masło");
+        ListaProduktow.addLast("Banany");
+        ListaProduktow.addLast("Ser");
+        ListaProduktow.addLast("Pieczarki");
+        ListaProduktow.addLast("Chleb");
+        ListaProduktow.addLast("Rzodkiewka");
+        ListaProduktow.addLast("Kawa");
+        ListaProduktow.addLast("Herbata");
+        ListaProduktow.addLast("Szlugi");
+        ListaProduktow.addLast("BMW");
+    }
+}
